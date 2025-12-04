36028b69cce8f443663f6893028c7a2daadace97
karim emad <sim.KarimEmad69873@alexu.edu.eg>
2025-12-04 03:35:31 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { Routes, Route } from "react-router-dom";
+// import { Routes, Route } from "react-router-dom";
+// import Navbar from "./components/Navbar";
+// import Drinks from "./components/Drinks";
+// import Hero from "./components/Hero";
+// import Food from "./components/Food";
+// import Coffee from "./components/Coffee";
+// export default function App() {
+//   return (
+//     <div className="bg-black min-h-screen text-white">
+//       <Navbar />
+
+//       <Routes>
+//         <Route path="/Hero" element={<Hero />} />
+//         <Route path="/drinks" element={<Drinks />} />
+//         <Route path="/Food" element={<Food />} />
+//         <Route path="/Coffee" element={<Coffee />} />
+//       </Routes>
+//     </div>
+//   );
+// }
+import { Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Drinks from "./components/Drinks";
 import Hero from "./components/Hero";
 import Food from "./components/Food";
 import Coffee from "./components/Coffee";
+
 export default function App() {
   return (
     <div className="bg-black min-h-screen text-white">
       <Navbar />
 
       <Routes>
+        <Route path="/" element={<Navigate to="/Hero" replace />} />
         <Route path="/Hero" element={<Hero />} />
         <Route path="/drinks" element={<Drinks />} />
         <Route path="/Food" element={<Food />} />
